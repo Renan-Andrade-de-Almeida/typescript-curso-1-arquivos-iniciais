a477a92b7f04e870fad3b966cac3b06e0bc9d6f3
Renan Almeida <101940943+Renan-Andrade-de-Almeida@users.noreply.github.com>
2022-08-31 11:28:38 -0300

    Initial commit

---
+++ app/controllers/negociacao-controller.ts
@@ -0,0 +1,41 @@
+import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
+
+export class NegociacaoController {
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+    private negociacioes = new Negociacoes;
+    private negociacoesView = new NegociacoesView('#negociacoesView');
+
+    constructor(){
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+        this.negociacoesView.update(this.negociacioes);
+    }
+
+    criaNegociacao(): Negociacao{
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+
+    adiciona(): void{
+        const negociacao = this.criaNegociacao();
+        this.negociacioes.adiciona(negociacao);
+        this.negociacoesView.update(this.negociacioes);
+        this.limparFormulario();
+    }
+
+    limparFormulario(): void{
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+
+}
---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,32 @@
+import { Negociacao } from "../models/negociacao.js";
+import { Negociacoes } from "../models/negociacoes.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
+export class NegociacaoController {
+    constructor() {
+        this.negociacioes = new Negociacoes;
+        this.negociacoesView = new NegociacoesView('#negociacoesView');
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+        this.negociacoesView.update(this.negociacioes);
+    }
+    criaNegociacao() {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        this.negociacioes.adiciona(negociacao);
+        this.negociacoesView.update(this.negociacioes);
+        this.limparFormulario();
+    }
+    limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+}
